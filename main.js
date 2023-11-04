1f5be9d31e7ff8bc2b2587a08139c9620c6f3b5e
Lai Duy Nghia <laiduynghia21@gmail.com>
2023-11-04 16:00:51 +0700

    Update code 4/11/23

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-function searchToggle(obj, evt) {
-  var container = $(obj).closest(".search-wrapper");
-  if (!container.hasClass("active")) {
-    container.addClass("active");
-    evt.preventDefault();
-  } else if (
-    container.hasClass("active") &&
-    $(obj).closest(".input-holder").length == 0
-  ) {
-    container.removeClass("active");
-    // clear input
-    container.find(".search-input").val("");
-  }
+// Danh sách các liên kết ảnh
+var imageLinks = [
+  "https://1.bp.blogspot.com/-W4QDVTougyE/Xr_N8wV-oYI/AAAAAAAAbT8/k239yXOFZO4E4dXkyw39B7AH0tVSqUu2wCLcBGAsYHQ/s1600/26120a8096e556cdecb60f59cbc4ae9f_mp4.gif",
+  "https://i.makeagif.com/media/12-29-2016/OGSfFD.gif",
+];
+
+var imgElement = document.getElementById("changingImage");
+var currentImageIndex = 0;
+
+// Hàm thay đổi liên kết ảnh
+function changeImage() {
+  imgElement.src = imageLinks[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % imageLinks.length;
 }
+
+// Đặt thời gian thay đổi liên kết ảnh sau mỗi 3 giây
+setInterval(changeImage, 2000);
