8640b816ec6d92d1a070d081405d73eff02a0ca4
Lai Duy Nghia <laiduynghia21@gmail.com>
2023-12-05 17:49:32 +0700

    update nghia ko co gì

--- demo.java
+++ demo.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * demo
  */
@@ -5,26 +8,29 @@ public class demo {
 
     public static void main(String[] args) {
 
-        String phoneNumber = "++6377888688";
-        String regex = "^(\\+)?\\d{10,12}$";
-        System.out.println( phoneNumber.matches(regex)); 
+        // String phoneNumber = "++6377888688";
+        // String regex = "^(\\+)?\\d{10,12}$";
+        // System.out.println( phoneNumber.matches(regex)); 
 
         
-        String cleanedNumber = phoneNumber.replaceAll("[^0-9]", "");
-        System.out.println(cleanedNumber);
+        // String cleanedNumber = phoneNumber.replaceAll("[^0-9]", "");
+        // System.out.println(cleanedNumber);
 
-        if (!(phoneNumber.startsWith("046") || phoneNumber.startsWith("84"))) {
-            String phoneCode = "84";
-            phoneNumber = phoneCode + phoneNumber.substring(1);
-        }
+        // if (!(phoneNumber.startsWith("046") || phoneNumber.startsWith("84"))) {
+        //     String phoneCode = "84";
+        //     phoneNumber = phoneCode + phoneNumber.substring(1);
+        // }
 
-        if (!(cleanedNumber.startsWith("046") || cleanedNumber.startsWith("84"))) {
-            String phoneCode = "84";
-            cleanedNumber = phoneCode + cleanedNumber.substring(1);
-        }
-        System.out.println(phoneNumber);
-        System.out.println(cleanedNumber);
+        // if (!(cleanedNumber.startsWith("046") || cleanedNumber.startsWith("84"))) {
+        //     String phoneCode = "84";
+        //     cleanedNumber = phoneCode + cleanedNumber.substring(1);
+        // }
+        // System.out.println(phoneNumber);
+        // System.out.println(cleanedNumber);
 
-        
+        LocalDateTime temp = LocalDateTime.now();
+        LocalDate a = temp.toLocalDate();
+        System.out.println("- LocalDateTime: " + temp);
+        System.out.println("- DateTime: " + a);
     }
 }
